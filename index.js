b0d9187e58676bd8d7c03954b1986984ecb4dd77
Den Kezlia <xian.den@gmail.com>
2016-04-06 02:12:19 +0300

    created base server with jade template

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+var blog = [{
+        id: '1',
+        name: 'Init title',
+        author: 'Den Kezlia',
+        date: '06.05.2015',
+        content: 'Hey you'
+    }, {
+        id: '2',
+        name: 'Processing title',
+        author: 'Den Kezlia',
+        date: '06.05.2015',
+        content: 'Hey you!'
+    }, {
+        id: '3',
+        name: 'Completed title',
+        author: 'Den Kezlia',
+        date: '06.05.2015',
+        content: 'Good buy.'
+    }];
+
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('pages/index', {blog: blog});
 });
 
 app.listen(3000);
