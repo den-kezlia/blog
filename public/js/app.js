91afb1afe8f2fb46588c61e72c2761ad26da22c5
Den Kezlia <xian.den@gmail.com>
2016-07-26 16:11:45 +0300

    updated site design

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+App = {
+    init: function() {
+        this.BGLinkStyles();
+    },
+
+    BGLinkStyles: function() {
+        var item = $('.js-bgstyle');
+
+        item.on('mousemove', function(e) {
+            var element = $(this);
+            var top = e.pageY - element.offset().top;
+            var left = e.pageX - element.offset().left;
+            var colorOne = '#212121';
+            var colorTwo = '#272727';
+            var radius = 450;
+
+            if (element.hasClass('js-bgstyle__small')) {
+                radius = 150;
+                colorOne = '#252525';
+                colorTwo = '#171717';
+            }
+
+            element.css('background',
+                'radial-gradient(' + radius + 'px at ' + left + 'px ' + top + 'px, ' + colorOne +', ' + colorTwo + ')');
+        });
+        item.hover(function() {}, function() {
+            $(this).css('background', 'none');
+        });
+    }
+};
+
+App.init();
