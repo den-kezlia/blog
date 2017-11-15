8d7c3c5d00339ee26e1f84bd4c41083860f63fee
Den Kezlya <dkezlya@lyonscg.com>
2017-11-15 14:54:58 +0200

    fixed Login form fields value

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,11 @@ module.exports = function (app, passport) {
     /*app.get('/login', function(req, res, next) {
         res.render('user/login', {message: req.flash('loginMessage')});
     });*/
-    /*app.post('/login', passport.authenticate('login', {
+    app.post('/login', passport.authenticate('login', {
         successRedirect: '/profile',
         failureRedirect: '/login',
         failureMessage: true
-    }));*/
+    }));
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
